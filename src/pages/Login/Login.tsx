caee05014b87944c6d1189d40972784bf585faa3
Nycollas Kaique <nycollaskaique@hotmail.com>
2024-05-28 17:20:27 -0300

    :sparkles: feat: Página de login e organização dos estilos css dessa página

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,72 @@
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
+const initialValues = {
+  email: "",
+  password: "",
+};
+const validationSchema = Yup.object({
+  email: Yup.string().email("Insira um e-mail válido").required("Obrigatório"),
+  password: Yup.string()
+    .min(6, "Senha muito curta")
+    .max(50, "Senha muito longa")
+    .required("Obrigatório"),
+});
+function handleLogin() {
+  console.log("submit");
+}
+export function Login() {
+  return (
+    <div className="w-full h-[100vh] flex bg-orange">
+      <div className="container-login glassmorphism-container">
+        <img
+          src="src\assets\logo-nutrify.png"
+          alt="Nutrify logo"
+          className="w-[60px] h-auto"
+        />
+        <h1 className="font-bold text-orange text-4xl">Nutrify</h1>
+        <Formik
+          enableReinitialize
+          initialValues={initialValues}
+          onSubmit={handleLogin}
+          validationSchema={validationSchema}
+        >
+          {() => (
+            <Form className="container-flex-col">
+              <div>E-mail</div>
+              <Field
+                autoComplete="off"
+                name="email"
+                type="email"
+                placeholder="E-mail"
+                className="input-light-text-color p-2 rounded-md glassmorphism-container"
+              />
+              <p className="text-orange text-sm font-bold">
+                <ErrorMessage name="email" />
+              </p>
+
+              <div>Senha</div>
+              <Field
+                name="password"
+                type="password"
+                placeholder="Senha"
+                className="input-light-text-color p-2 rounded-md glassmorphism-container"
+              />
+              <p className="text-orange text-sm font-bold">
+                <ErrorMessage name="password" />
+              </p>
+
+              <div className="container-flex-row  mt-5 flex-wrap ">
+                <button type="submit" className="button-orange">
+                  Login
+                </button>
+                <button type="button" className="button-orange">
+                  Registrar
+                </button>
+              </div>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </div>
+  );
+}
