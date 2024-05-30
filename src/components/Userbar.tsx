26ae4fd1c7e2cb438c1bd9fe16a1715a495fbbe9
Nycollas Kaique <nycollaskaique@hotmail.com>
2024-05-30 11:55:39 -0300

    :sparkles: feat: User bar com nome e foto do usuário

---
+++ src/components/Userbar.tsx
@@ -0,0 +1,15 @@
+import { useContext } from "react";
+import { Context } from "../context";
+export default function Userbar() {
+  const { user } = useContext(Context)!;
+  return (
+    <div className="flex items-center justify-between p-2 gap-2 rounded-lg bg-[var(--primary-orange)] min-w-[200px] w-fit h-[90px] shadowblack">
+      <img
+        src={user?.avatarUrl ? user.avatarUrl : "src/assets/avatar.png"}
+        alt=""
+        className="w-[74px] h-auto object-fill p-0"
+      />
+      <p className="font-bold text-xl text-white">Bem vindo {user?.nome}!</p>
+    </div>
+  );
+}
