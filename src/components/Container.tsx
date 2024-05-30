83b55399a6dea0bdd74a2b65f36608efd65783de
Nycollas Kaique <nycollaskaique@hotmail.com>
2024-05-30 11:57:29 -0300

    :sparkles: feat: Container que exibe o conteúdo principal da página

---
+++ src/components/Container.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react";
+
+interface ContainerProps {
+  children: ReactNode;
+}
+export default function Container({ children }: ContainerProps) {
+  return (
+    <div
+      className="w-full flex flex-col bg-white border-l shadowblack border-zinc-200 rounded-r-lg p-3 gap-6"
+      style={{ height: "calc(100vh - 32px)" }}
+    >
+      {children}
+    </div>
+  );
+}
