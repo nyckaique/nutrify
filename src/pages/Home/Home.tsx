1d707972a85239a21c84f3eb434aa609e70c8909
Nycollas Kaique <nycollaskaique@hotmail.com>
2024-05-30 11:57:55 -0300

    :sparkles: feat: Página Home com o container, userbar e pagetitle

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,16 @@
+import Container from "../../components/Container";
+import Pagetitle from "../../components/Pagetitle";
 import Sidebar from "../../components/Sidebar";
+import Userbar from "../../components/Userbar";
 export function Home() {
   // const { } = useContext(Context)!;
   return (
-    <div className="min-h-screen p-4 bg-orange">
+    <div className="min-h-screen p-4 bg-orange flex">
       <Sidebar pageid={1} />
+      <Container>
+        <Userbar />
+        <Pagetitle titulo="Pacientes" />
+      </Container>
     </div>
   );
 }
